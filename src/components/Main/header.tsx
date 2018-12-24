1b37b0f69ba6fa3e97218b50132659ad37a2d6fa
Sajjad Mohammadi <radhoosh.user2@gmail.com>
2018-12-24 12:21:51 +0330

    edit header

--- src/components/Main/header.tsx
+++ src/components/Main/header.tsx
@@ -5,28 +5,67 @@ import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { Link } from 'react-router-dom';
 
 const styles = (theme: Theme) =>
     createStyles({
         root: {
             flexGrow: 1,
         },
+        paper: {
+            marginTop: theme.spacing.unit * 8,
+            padding: theme.spacing.unit * 2,
+            textAlign: 'left',
+            color: theme.palette.text.secondary,
+        },
+        title: {
+            marginLeft: theme.spacing.unit * 2,
+            display: 'none',
+            [theme.breakpoints.up('sm')]: {
+                display: 'block',
+            },
+        },
     });
 
 class Index extends React.Component<WithStyles<typeof styles>> {
-  
+
 
     render() {
+        const { classes } = this.props;
         return (
-            <div className={this.props.classes.root}>
-                <AppBar position="static" color="default">
+            <div className={classes.root}>
+                <AppBar position="fixed" color="primary">
                     <Toolbar>
-                        <Typography variant="h6" color="inherit">
-                            Main
-                         </Typography>
+                        <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+
+                            <Link type="secondary" to="/" >
+                                خانه
+                               </Link>
+                        </Typography>
+                        <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+
+                            <Link type="secondary" to="/blog" >
+                                وبلاگ
+                               </Link>
+                        </Typography>
+
+                        <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+                            <Link type="secondary" to="/about" >
+                                درباره ما
+                               </Link>
+                        </Typography>
+
                     </Toolbar>
                 </AppBar>
-                {this.props.children}
+                <Grid container spacing={0}>
+                    <Grid item xs={12} className={classes.paper}>
+                        {this.props.children}
+                    </Grid>
+                </Grid>
+
+
             </div>
         );
     }
